f5e2e78411651a7356e95acb4df6a8bd10f995db
augustopdro <rm94990@fiap.com.br>
2022-09-14 18:28:28 -0300

    Fix turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -80,7 +80,7 @@ public class ChessMatch {
     }
 
     private void nextTurn() {
-        turn++;
+        if (currentPlayer == Color.BLACK) turn++;
         currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
